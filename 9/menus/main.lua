8d981c6183ef0d16c948c0ae9e6b62e06835864e
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-09-18 04:10:52 -0500

    Move some options to device menu

--- 9/menus/main.lua
+++ 9/menus/main.lua
@@ -33,32 +33,9 @@ return {
 		menu:use(title)
 	end},
 
-	{'Lua Console', function()
-		term.clear()
-		term.setCursorPos(1, 1)
-		shell.run('lua')
-	end},
-
-	{'Restart Server', function(menu)
-		rednet.send(serverID, 'restart', 'commands')
-		menu:message('Server restarted.', 2)
-	end},
-
-	{'Test Network Range', function(menu)
-		while true do
-			rednet.broadcast('ping')
-			local senderID, msg, distance = rednet.receive(nil, 1)
-
-			if senderID then
-				menu:message('In range.')
-				sleep(1)
-			else
-				menu:message('Out of range.')
-			end
-		end
-	end},
-
-	{'Shutdown', function()
-		os.shutdown()
+	{'Device', function(menu)
+		local title = 'Device Menu'
+		menu:add(title, dofile('/menus/device.lua'))
+		menu:use(title)
 	end}
 }
