3ff17b62a602656884d00374110ad99d4bdcbc2e
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-10-02 01:55:40 -0500

    Update command server
    - Refactor procedures to return response message
    - Update 'update' module

--- 10/procedures.lua
+++ 10/procedures.lua
@@ -11,50 +11,62 @@ return {
 
 	time_morning = function()
 		commands.exec('time set 0')
+		return 'true'
 	end,
 
 	time_noon = function()
 		commands.exec('time set 6000')
+		return 'true'
 	end,
 
 	time_evening = function()
 		commands.exec('time set 9000')
+		return 'true'
 	end,
 
 	time_night = function()
 		commands.exec('time set 14000')
+		return 'true'
 	end,
 
 	weather_clear = function()
 		commands.exec('weather clear')
+		return 'true'
 	end,
 
 	weather_rain = function()
 		commands.exec('weather rain')
+		return 'true'
 	end,
 
 	weather_thunder = function()
 		commands.exec('weather thunder')
+		return 'true'
 	end,
 
 	tp_amita_entrance = function()
 		commands.exec('tp ' .. username .. ' 351 74 260')
+		return 'true'
 	end,
 
 	tp_amita_serverroom = function()
 		commands.exec('tp ' .. username .. ' 351 85 337')
+		return 'true'
 	end,
 
 	tp_docks = function()
 		commands.exec('tp ' .. username .. ' 416 64 195')
+		return 'true'
 	end,
 
 	tp_quartier_de_lagnol = function()
 		commands.exec('tp ' .. username .. ' 356 71 561')
+		return 'true'
 	end,
 
 	tp_farm = function()
 		commands.exec('tp ' .. username .. ' 223 67 300')
+		return 'true'
 	end,
 
 	restart = function()
