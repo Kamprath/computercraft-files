f51264f713e1fa6be8075eb68c811bbc70f111db
Johnny Kamprath <johnny.kamprath@archrival.com>
2017-02-08 22:57:19 -0600

    Add 'Watchtower'  to command device's teleport menu

--- 9/menus/teleport_amida.lua
+++ 9/menus/teleport_amida.lua
@@ -12,6 +12,10 @@ return {
     {'Docks', function()
 		rednet.send(serverID, 'tp_docks', protocol)
 	end},
+
+    {'Watchtower', function()
+        rednet.send(serverID, 'tp 424 83 402', protocol)
+    end},
     
     {'Back', function(menu)
         menu:back()
